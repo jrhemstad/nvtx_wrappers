b015607bdadb65c4459d2f585456d26f1ce44c6b
Jake Hemstad <jhemstad@nvidia.com>
2020-02-17 13:18:20 -0600

    Update color usage.

--- main.cu
+++ main.cu
@@ -10,9 +10,9 @@ struct my_domain {
 using my_thread_range = nvtx::domain_thread_range<my_domain>;
 
 int main(void){
-    nvtx::domain_thread_range<my_domain> r{"msg", nvtx::argb_color{0}};
+    nvtx::domain_thread_range<my_domain> r{"msg", nvtx::Color{nvtx::RGB{255,255,255}}};
 
-    nvtx::thread_range r2{"msg", nvtx::argb_color{0}};
+    nvtx::thread_range r2{"msg", nvtx::Color{nvtx::ARGB{255,255,255,255}}};
 
-    my_thread_range my_range{"msg", nvtx::argb_color{0}};
+    my_thread_range my_range{"msg", nvtx::Color{0}};
 }
