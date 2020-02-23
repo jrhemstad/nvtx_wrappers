f0d3c6aa5d346ecd0bf6e69e023cb667533263e2
Jake Hemstad <jhemstad@nvidia.com>
2020-02-23 14:21:28 -0600

    Basic callback working.

--- tests/nvtx_tests.cpp
+++ tests/nvtx_tests.cpp
@@ -14,30 +14,48 @@
  * limitations under the License.
  */
 
-#include "cupti_nvtx_injection.hpp"
-
 #include <gtest/gtest.h>
 
+#include <nvToolsExtSync.h>
 #include <nvtx.hpp>
+
+#include <cupti.h>
+#include <generated_nvtx_meta.h>
 
 #include <iostream>
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 
+void CUPTIAPI my_callback(void *userdata, CUpti_CallbackDomain domain,
+                          CUpti_CallbackId cbid, const void *cbdata) {
+  std::cout << "my callback\n";
+  const CUpti_NvtxData *nvtxInfo = (CUpti_NvtxData *)cbdata;
+
+  if ((domain == CUPTI_CB_DOMAIN_NVTX) &&
+      (cbid == CUPTI_CBID_NVTX_nvtxRangePushA)) {
+    nvtxRangePushA_params * p = (nvtxRangePushA_params *)nvtxInfo->functionParams;
+    std::cout << "push message: " << p->message << std::endl;
+  }
+}
+
 struct NVTX_Test : public ::testing::Test {
   NVTX_Test() {
     // Get path to `libcupti.so` from the `CUPTI_PATH` definition specified as a
     // compile argument
-    constexpr char const* cupti_path = TOSTRING(CUPTI_PATH);
+    constexpr char const *cupti_path = TOSTRING(CUPTI_PATH);
 
     // Inject CUPTI into NVTX
     setenv("NVTX_INJECTION64_PATH", cupti_path, 1);
+
+    // Register `my_callback` to be invoked for all NVTX APIs
+    CUpti_SubscriberHandle subscriber;
+    cuptiSubscribe(&subscriber, (CUpti_CallbackFunc)my_callback, nullptr);
+    cuptiEnableDomain(1, subscriber, CUPTI_CB_DOMAIN_NVTX);
   }
 };
 
 TEST_F(NVTX_Test, first) {
   nvtxRangePushA("test");
   nvtxRangePop();
-  std::cout << "First\n";
 }
