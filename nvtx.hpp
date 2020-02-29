dc57b6d355c94f439b064581e0ed758a6a66309d
Jake Hemstad <jhemstad@nvidia.com>
2020-02-29 15:34:35 -0600

    Trying to get mainpage to work.

--- nvtx.hpp
+++ nvtx.hpp
@@ -22,10 +22,11 @@
 /**
  * @file nvtx.hpp
  *
+ * \mainpage
+ * 
  * @brief Provides C++ constructs making the NVTX library safer and easier to
  * use with zero overhead.
  * 
- * \mainpage
  *
  * The NVTX library provides C APIs for users to annotate their code to aid in
  * performance profiling and optimization. One of the most commonly  used NVTX
