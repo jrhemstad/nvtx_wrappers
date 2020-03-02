03db9cdc380ae943ea997c9bb871838e039c9292
Jake Hemstad <jhemstad@nvidia.com>
2020-03-02 17:06:53 -0600

    Quick start.

--- docs/html/search/all_5.js
+++ docs/html/search/all_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['get',['get',['../classnvtx_1_1Domain.html#ab42ea71ed0303a899443aa7edfb1b320',1,'nvtx::Domain::get()'],['../classnvtx_1_1NamedCategory.html#af126f25675eaca24ba4a8c3599b08aa8',1,'nvtx::NamedCategory::get()'],['../classnvtx_1_1RegisteredMessage.html#af6758cf7f01741d0a8ed8b03d0de270e',1,'nvtx::RegisteredMessage::get()'],['../classnvtx_1_1EventAttributes.html#aef7f966718345929deffb2d5e9a8c513',1,'nvtx::EventAttributes::get()']]],
-  ['get_5fhandle',['get_handle',['../classnvtx_1_1RegisteredMessage.html#ab387647cf974a88f17722fc1b7898017',1,'nvtx::RegisteredMessage']]],
+  ['get_5fhandle',['get_handle',['../classnvtx_1_1RegisteredMessage.html#aa841c76f8f1ab25260e8c5f1b11a0f66',1,'nvtx::RegisteredMessage']]],
   ['get_5fid',['get_id',['../classnvtx_1_1Category.html#a9ab86c5e2e220ab2d25e15cff95e7406',1,'nvtx::Category']]],
   ['get_5ftype',['get_type',['../classnvtx_1_1Color.html#a61c2082f5163fc4ed67813bc3fae216e',1,'nvtx::Color::get_type()'],['../classnvtx_1_1Message.html#a8da2896bb938cf63113eae6695f7e102',1,'nvtx::Message::get_type()'],['../classnvtx_1_1Payload.html#a4553733838fcfbb1ef80ab34ad5259eb',1,'nvtx::Payload::get_type()']]],
   ['get_5fvalue',['get_value',['../classnvtx_1_1Color.html#a30a438e5a004508dff453ab5e25ede4c',1,'nvtx::Color::get_value()'],['../classnvtx_1_1Message.html#a5e9202e26c6b3e01d9f988085606f3eb',1,'nvtx::Message::get_value()'],['../classnvtx_1_1Payload.html#aa0f0e78e7f63ded789a1bac15972781a',1,'nvtx::Payload::get_value()']]],
